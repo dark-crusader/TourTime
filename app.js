326c61c7bb0cab59d0cf057a0e36934392acaf9e
Shubham Anand <shubham.rubal31@gmail.com>
2020-07-24 21:58:38 +0530

    Added comments feature for users

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const Comment = require('./models/comment');
 const seedDB = require('./seeds');
-
-// Seeding database with initial data
-seedDB();
 
 mongoose.connect('mongodb://localhost:27017/tour_time', {useNewUrlParser: true, useUnifiedTopology: true});
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public/images/"));
 
+// Seeding database with initial data
+seedDB();
 
 app.get('/', (req,res) => {
     res.render('index');
@@ -24,7 +24,7 @@ app.get('/campgrounds', (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render('campgrounds', {camps: camps});
+            res.render('campgrounds/campgrounds', {camps: camps});
         }
     });
 });
@@ -40,14 +40,14 @@ app.post('/campgrounds', (req, res) => {
             console.log(err);
         } else {
             // Redirect to /campgrounds
-            res.redirect('/campgrounds');
+            res.redirect('campgrounds/campgrounds');
         }
     });
 });
 
 // Route to add a new Campground
 app.get('/campgrounds/new', (req,res) => {
-    res.render('new');
+    res.render('campgrounds/new');
 });
 
 // Route to fetch info on a specific campground
@@ -57,10 +57,44 @@ app.get('/campgrounds/:id', (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render('show', {campground: camp});
+            res.render('campgrounds/show', {campground: camp});
         }
     });
 });
+
+// Routes for comments
+app.get('/campgrounds/:id/comments/new', (req, res) => {
+    // Get campground id
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('comments/new', {campground: campground});
+        }
+    });
+});
+
+app.post('/campgrounds/:id/comments', (req, res) => {
+    // Create and add new comment to campground
+    // redirect to camground
+    Campground.findById(req.params.id, (err, campground) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/campgrounds');
+        } else {
+            Comment.create(req.body.comment, (err, comment) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect(`/campgrounds/${campground._id}`);
+                }
+            });
+        }
+    });
+});
+
 
 app.listen(port = 8080, () => {
     console.log(`App stated at localhost:${port}`);
